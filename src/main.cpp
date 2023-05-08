34e39a8ac1b2882ed45e7b60ea5f70ac38505e0c
4t-2 <zbfddx@protonmail.com>
2023-05-08 22:58:46 +0100

    Added cube and camera controls

--- src/main.cpp
+++ src/main.cpp
@@ -12,11 +12,48 @@ int main()
 	event.setWindow(window);
 
 	agl::ShaderBuilder sbFrag;
-	sbFrag.setDefaultFrag();
+	{
+		using namespace agl;
+
+		val UVcoord	  = sbFrag.addIn(agl::vec2, "UVcoord");
+		val fragColor = sbFrag.addIn(agl::vec4, "fragColor");
+		val pos		  = sbFrag.addIn(agl::vec3, "pos");
+
+		val color = sbFrag.addOut(agl::vec4, "color");
+
+		val myTextureSampler = sbFrag.addUniform(agl::sampler2D, "myTextureSampler");
+
+		sbFrag.setMain({
+			color = agl::val("vec4(pos, 1.)") * agl::val("texture(myTextureSampler, UVcoord)"), //
+		});
+	}
+
 	std::string fragSrc = sbFrag.getSrc();
 
 	agl::ShaderBuilder sbVert;
-	sbVert.setDefaultVert();
+	{
+		using namespace agl;
+
+		val position = sbVert.addLayout(0, agl::vec3, "position");
+		val vertexUV = sbVert.addLayout(1, agl::vec2, "vertexUV");
+
+		val transform		 = sbVert.addUniform(agl::mat4, "transform");
+		val mvp				 = sbVert.addUniform(agl::mat4, "mvp");
+		val shapeColor		 = sbVert.addUniform(agl::vec3, "shapeColor");
+		val textureTransform = sbVert.addUniform(agl::mat4, "textureTransform");
+
+		val UVcoord	  = sbVert.addOut(agl::vec2, "UVcoord");
+		val fragColor = sbVert.addOut(agl::vec4, "fragColor");
+		val pos		  = sbVert.addOut(agl::vec3, "pos");
+
+		sbVert.setMain({
+			UVcoord				  = val("vec2((textureTransform") * val("vec4(vertexUV, 1, 1)).xy)"), //
+			fragColor			  = val("vec4(shapeColor, 1)"),										  //
+			pos					  = position,
+			val(val::gl_Position) = mvp * transform * val("vec4(position, 1)"), //
+		});
+	}
+
 	std::string vertSrc = sbVert.getSrc();
 
 	agl::Shader shader;
@@ -26,19 +63,22 @@ int main()
 	shader.use();
 
 	agl::Camera camera;
-	camera.setView({0, 0, 10}, {0, 0, 0}, {0, 1, 0});
-	camera.setOrthographicProjection(0, 1920, 1080, 0, 0.1, 100);
+	camera.setView({5, 5, 5}, {0, 0, 0}, {0, 1, 0});
+	camera.setPerspectiveProjection(45, 1920. / 1080., .1, 100);
+
+	float angle = 0;
+	float scale = 5;
 
 	window.updateMvp(camera);
 
 	agl::Texture blank;
 	blank.setBlank();
 
-	agl::Rectangle rectangle;
-	rectangle.setColor(agl::Color::Cyan);
-	rectangle.setTexture(&blank);
-	rectangle.setPosition({100, 100});
-	rectangle.setSize({1720, 880});
+	agl::Cuboid cuboid;
+	cuboid.setColor(agl::Color::Cyan);
+	cuboid.setTexture(&blank);
+	cuboid.setPosition({0, 0, 0});
+	cuboid.setSize({1, 1, 1});
 
 	while (!event.windowClose())
 	{
@@ -46,9 +86,34 @@ int main()
 
 		window.clear();
 
-		window.drawShape(rectangle);
+		window.drawShape(cuboid);
 
 		window.display();
+
+		if (event.isKeyPressed(XK_Left))
+		{
+			angle += PI / 60;
+		}
+
+		if (event.isKeyPressed(XK_Right))
+		{
+			angle -= PI / 60;
+		}
+
+		if(event.isKeyPressed(XK_Up))
+		{
+			scale /= 1.05;
+		}
+		
+		if(event.isKeyPressed(XK_Down))
+		{
+			scale *= 1.05;
+		}
+
+		agl::Vec<float, 2> normal = agl::pointOnCircle(angle);
+
+		camera.setView(agl::Vec<float, 3>{normal.x, 1, normal.y} * scale, {0, 0, 0}, {0, 1, 0});
+		window.updateMvp(camera);
 	}
 
 	blank.deleteTexture();
